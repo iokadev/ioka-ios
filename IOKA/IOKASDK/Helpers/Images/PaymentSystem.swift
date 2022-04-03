0bffeb79e56de6720380811735bfcafd30878cbf
karaevablay <94847114+karaevablay@users.noreply.github.com>
2022-04-03 11:21:55 +0600

    feature: add payment system icons for merchants

---
+++ IOKA/IOKASDK/Helpers/Images/PaymentSystem.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentSystem.swift
+//  IOKA
+//
+//  Created by ablai erzhanov on 03.04.2022.
+//
+
+import Foundation
+import UIKit
+
+// - This is public enum for merchants to get bank payment system icons
+enum IokaIcons {
+    static var AMERICAN_EXPRESS = UIImage(named: "AMERICAN_EXPRESS")
+    static var DINER_CLUB = UIImage(named: "DINER_CLUB")
+    static var MAESTRO = UIImage(named: "MAESTRO")
+    static var MASTERCARD = UIImage(named: "MASTERCARD")
+    static var MIR = UIImage(named: "MIR")
+    static var UNION_PAY = UIImage(named: "UNION_PAY")
+    static var VISA = UIImage(named: "VISA")
+}
