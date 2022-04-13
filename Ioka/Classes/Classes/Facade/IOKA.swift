0bf410263538ab0c0288ee8ab1731d6346418eeb
Timur Tabynbayev <timur.tabynbayev@gmail.com>
2022-04-13 14:38:27 +0600

    inline docs for Ioka

--- Ioka/Classes/Classes/Facade/IOKA.swift
+++ Ioka/Classes/Classes/Facade/IOKA.swift
@@ -9,19 +9,41 @@ import Foundation
 import UIKit
 
 
+/// Класс-фасад Ioka SDK. Реализован как синглтон.
+/// Предоставляет методы для инициализации SDK, запуска сценариев оплаты и сохранения карты, и работы с сохраненными картами.
 public class Ioka {
+    /// Синглтон
     static public let shared = Ioka()
     var setupInput: SetupInput?
     var theme: IokaTheme = .default
     var currentCoordinator: Coordinator?
     var locale: IokaLocale = .automatic
     
+    
+    /// Метод для инициализии SDK. Необходимо вызвать до того, как обращаться к любым другим методам этого класса.
+    ///
+    /// - Parameters:
+    ///   - apiKey: Публичный API-ключ, полученные при регистрации в качестве клиента Ioka
+    ///   - theme: Конфигурирует внешний вид экранов Ioka SDK. По умолчанию - объект IokaTheme.default.
+    ///   - locale: Конфигурирует локализацию текстов в Ioka SDK. По умолчанию - .automatic.
+    ///   Передавать другое значение нужно в том случае, если язык в вашем приложении выбирается вручную.
     public func setup(apiKey: String, theme: IokaTheme = .default, locale: IokaLocale = .automatic) {
         self.setupInput = SetupInput(apiKey: APIKey(key: apiKey), theme: theme )
         self.theme = theme
         updateLocale(locale: locale)
     }
     
+    /// Метод для запуска сценария оплаты новой картой. Показывает форму для ввода данных карты.
+    /// При необходимости проводит проверку 3DSecure. Отображает результат оплаты пользователю.
+    /// - Parameters:
+    ///   - sourceViewController: Объект UIViewController для экрана, с которого пользователь запускает оплату.
+    ///   Перед открытием формы для ввода данных карты Ioka SDK получает объект Order из Ioka API. Во время этого запроса
+    ///   поверх sourceViewController показывается индикатор загрузки и могут отображаться ошибки. Также с него происходит
+    ///   переход на экраны Ioka SDK.
+    ///   - orderAccessToken: Токен для доступа к заказу, который приложение получает от своего бэкенда.
+    ///   - completion: Замыкание, которое вызывается после того, как пользователь закрывает экран результата оплаты или
+    ///   любой экран до него. Принимает значение FlowResult: .succeeded - если оплата прошла успешно, .failed - если карта
+    ///   была отклонена, .cancelled - если пользователь закрыл экран оплаты или экран 3DSecure.
     public func startCheckoutFlow(sourceViewController: UIViewController, orderAccessToken: String, completion: @escaping(FlowResult) -> Void) {
         guard let setupInput = setupInput else {
             completion(.failed(DomainError.invalidTokenFormat))
@@ -45,6 +67,18 @@ public class Ioka {
         }
     }
     
+    /// Метод для запуска сценария оплаты сохраненной картой. Показывает индикатор загрузки, пока проходит оплата. При
+    /// необходимости показывает экран ввода CVV. Также при необходимости проводит проверку 3DSecure. Отображает результат
+    /// оплаты пользователю.
+    /// - Parameters:
+    ///   - sourceViewController: Объект UIViewController для экрана, с которого пользователь запускает оплату. Перед
+    ///   проведением оплаты Ioka SDK получает объект Order из Ioka API. Во время этого запроса, а также во время проведения
+    ///   оплаты, поверх sourceViewController показывается индикатор загрузки и могут отображаться ошибки. Также с него
+    ///   происходит переход на экраны Ioka SDK.
+    ///   - orderAccessToken: Токен для доступа к заказу, который приложение получает от своего бэкенда.
+    ///   - completion: Замыкание, которое вызывается после того, как пользователь закрывает экран результата оплаты или любой
+    ///   экран до него. Принимает значение FlowResult: .succeeded - если оплата прошла успешно, .failed - если карта была
+    ///   отклонена, .cancelled - если пользователь закрыл экран ввода CVV или экран 3DSecure.
     public func startCheckoutWithSavedCardFlow(sourceViewController: UIViewController, orderAccessToken: String, card: SavedCardDTO, completion: @escaping(FlowResult) -> Void) {
         guard let setupInput = setupInput else {
             completion(.failed(DomainError.invalidTokenFormat))
@@ -69,6 +103,16 @@ public class Ioka {
         }
     }
     
+    /// Метод для запуска сценария сохранения карты. Показывает форму для ввода данных карты. При необходимости проводит
+    /// проверку 3DSecure. Отображает результат сохранения пользователю.
+    /// - Parameters:
+    ///   - sourceViewController: Объект UIViewController для экрана, с которого пользователь запускает сохранение карты.
+    ///   С него происходит переход на экраны Ioka SDK.
+    ///   - customerAccessToken: Токен для доступа к пользователю, который приложение получает от своего бэкенда.
+    ///   - completion: Замыкание, которое вызывается после того, как пользователь закрывает экран сохранения. Принимает
+    ///   значение FlowResult: .succeeded - если оплата прошла успешно, .cancelled - если пользователь закрыл экран
+    ///   сохранения или экран 3DSecure, .failed в данном случае не приходит, так как при ошибке пользователь всегда
+    ///   остаётся на экране сохранения.
     public func startSaveCardFlow(sourceViewController: UIViewController, customerAccessToken: String, completion: @escaping(FlowResult) -> Void) {
         guard let setupInput = setupInput else {
             completion(.failed(DomainError.invalidTokenFormat))
@@ -95,6 +139,10 @@ public class Ioka {
         }
     }
     
+    /// Метод для получения сохраненных карт пользователя
+    /// - Parameters:
+    ///   - customerAccessToken: Токен для доступа к пользователю, который приложение получает от своего бэкенда.
+    ///   - completion: Замыкание, в которое передаётся результат запроса сохраненных карт.
     public func getCards(customerAccessToken: String, completion: @escaping(Result<[SavedCardDTO], Error>) -> Void) {
         guard let setupInput = setupInput else {
             completion(.failure(DomainError.invalidTokenFormat))
@@ -110,6 +158,12 @@ public class Ioka {
         }
     }
     
+    /// Метод для удаления сохраненной карты пользователя
+    /// - Parameters:
+    ///   - customerAccessToken: Токен для доступа к пользователю, который приложение получает от своего бэкенда.
+    ///   - cardId: id карты, которую нужно удалить.
+    ///   - completion: Замыкание, в которое передаётся результат удаления карты. nil - если карта удалена успешно,
+    ///   error - если произошла ошибка.
     public func deleteSavedCard(customerAccessToken: String, cardId: String, completion: @escaping(Error?) -> Void) {
         
         guard let setupInput = setupInput else {
@@ -133,6 +187,8 @@ public class Ioka {
         }
     }
     
+    /// Переключает локаль для локализации текстов в ioka SDK. Необходимо вызвать, если пользователь переключил язык
+    /// в приложении.
     public func updateLocale(locale: IokaLocale) {
         self.locale = locale
     }
