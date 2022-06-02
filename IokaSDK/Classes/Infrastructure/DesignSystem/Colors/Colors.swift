351f2630bc30ea0f77356137992a301a2cedc849
karaevablay <94847114+karaevablay@users.noreply.github.com>
2022-06-02 18:50:12 +0600

    change bundle name

--- IokaSDK/Classes/Infrastructure/DesignSystem/Colors/Colors.swift
+++ IokaSDK/Classes/Infrastructure/DesignSystem/Colors/Colors.swift
@@ -46,7 +46,7 @@ final class IokaBundle {
         let myBundle = Bundle(for: IokaBundle.self)
 
         guard let resourceBundleURL = myBundle.url(
-            forResource: "Ioka", withExtension: "bundle")
+            forResource: "IokaSDK", withExtension: "bundle")
             else { fatalError("Ioka.bundle not found") }
 
         guard let resourceBundle = Bundle(url: resourceBundleURL)
